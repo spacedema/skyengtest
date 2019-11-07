eb5715ba7f1c022dc792ac4c6dcad6fb6f6d9901
sfilippov <sfilippov@ronte.io>
2019-11-07 21:21:31 +0300

    Initial Commit

---
+++ SkyEngTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyEngTest
+//
+//  Created by Сергей Филиппов on 07/11/2019.
+//  Copyright © 2019 spacedema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
